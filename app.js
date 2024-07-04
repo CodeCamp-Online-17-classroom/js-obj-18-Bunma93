6101867ca46adf68a189810e6312f4b7ec977c61
Bunma93 <bunma93@gmail.com>
2024-07-04 16:55:15 +0700

    Js-obj18

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // write code here
+let user = {
+    name: 'John',
+    sayHi: function () {
+      console.log(this.name);
+    }
+  };
+  
+  (user.sayHi)(); // *John เรียกใช้ method sayHi ที่มี console.log(this.name) 
+  //ซึ่ง this.name หมายถึง property name ใน object before the dot
